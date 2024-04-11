df95fccd4cdd43aa37c3738f043c279e292e5b8a
Rodolfo Antoniazzi <rodolfo@enseg.com>
2024-04-11 15:33:47 -0300

    Initial Commit

---
+++ FootballAgent/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  FootballAgent
+//
+//  Created by Rodolfo Antoniazzi on 11/04/24.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
